172a98c8ca88c86dd1c068a64e278e5548b00d39
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2021-11-20 14:23:58 +0200

    Add project files.

---
+++ Ihor_Lesson3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ihor_Lesson3
+{
+    class Program
+    {
+        static void MultiplicationTable()
+        {
+            for (int i = 2; i <= 9; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void AsciiTable()
+        {
+            for (int i = 0; i < 128; i++)
+            {
+                Console.WriteLine($"{(char)i}\t{i}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(char.IsLetter('5'));
+        }
+    }
+}
